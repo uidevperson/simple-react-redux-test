31a134595bdae94f22f33a1a2a40119a86c99625
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-04-19 15:20:36 +0100

    init

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// Combine Reducers
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibilityFilter'
+
+export default combineReducers({
+  todos,
+  visibilityFilter
+})
